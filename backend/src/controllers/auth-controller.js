f76d456d695ce89b3f459e40aea20e2b87faaae7
mubashir-ansari-cog <mubashir.ansari@cogencis.com>
2025-08-21 12:15:29 +0530

    remove key

--- backend/src/controllers/auth-controller.js
+++ backend/src/controllers/auth-controller.js
@@ -7,7 +7,7 @@ export const fetchMosque = TryCatchBlocker(async(req,res)=>{
 
 async function getAllMosque(){
     try {
-        const url = "https://maps.googleapis.com/maps/api/geocode/json?address=Byculla&key=AIzaSyC25BJFmqMXg9Uv7wBUiR-PrIQ4Y59SfkM%0A"
+        const url = "https://maps.googleapis.com/maps/api/geocode/json?address=Byculla&key="
         const fetchData = await axios.get('')
     } catch (error) {
         
