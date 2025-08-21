cafebe5a66fe456c9c9372f204bb52e11498f78a
mubashir-ansari-cog <mubashir.ansari@cogencis.com>
2025-08-21 11:53:26 +0530

    added the output json

--- backend/src/controllers/auth-controller.js
+++ backend/src/controllers/auth-controller.js
@@ -1,5 +1,15 @@
+import axios from "axios";
 import { TryCatchBlocker } from "../utils/TryCatchWrapper";
 
 export const fetchMosque = TryCatchBlocker(async(req,res)=>{
-    
+
 })
+
+async function getAllMosque(){
+    try {
+        const url = "https://maps.googleapis.com/maps/api/geocode/json?address=Byculla&key=AIzaSyC25BJFmqMXg9Uv7wBUiR-PrIQ4Y59SfkM%0A"
+        const fetchData = await axios.get('')
+    } catch (error) {
+        
+    }
+}
